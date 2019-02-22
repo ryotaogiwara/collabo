bb1e1267f73913a04f43a075a6f004a182f934fb
ryotaogiwara <g00d03wzwyr16@gmail.com>
2019-02-22 10:50:47 +0000

    create edit.html

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '5ffa09e574a79303e22414b4f0ea7134943178016d051a5610ea97a31a4e9e40f3f4c53cb47d46457af330cf6d6b609f4438a0db3b74988e93dfb7c0918aea12'
